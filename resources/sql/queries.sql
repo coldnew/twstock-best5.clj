0283908d1270e05a781d47004c8a5d4051632f71
Yen-Chin Lee <coldnew.tw@gmail.com>
2018-04-27 11:16:52 +0800

    use sqlite instead of datascript
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

---
+++ resources/sql/queries.sql
@@ -0,0 +1,5 @@
+-- name: best5-setup-db!
+-- create table we need
+CREATE TABLE IF NOT EXISTS stocklist (
+       url TEXT
+)
--- src/coldnew/best5/db.clj
+++ src/coldnew/best5/db.clj
@@ -1,56 +1,15 @@
 (ns coldnew.best5.db
-  (:require [datascript.core :as d]
-            [datascript.transit :as dt]
-            [coldnew.best5.stock :as stock]))
+  (:require ;; [coldnew.best5.stock :as stock]
+            [yesql.core :refer [defqueries]]))
 
-;; https://github.com/tonsky/datascript/issues/152
-;;
-;; The only value type supported ATM is :db.type/ref. For anything else, just
-;; don’t specify :db/valueType
-;; (def schema {:代號     {:db/unique :db.unique/identity}
-;;              :名稱     {:db/unique :db.unique/identity}
-;;              :ISINCode {:db/unique :db.unique/identity}
-;;              :上市日   {:db/unique :db.unique/identity}
-;;              :市場別   {:db/unique :db.unique/identity}
-;;              :產業別   {:db/unique :db.unique/identity}
-;;              :CFICode  {:db/unique :db.unique/identity}
-;;              :備註     {:db/unique :db.unique/identity}})
+(def conn {:classname   "org.sqlite.JDBC"
+           :subprotocol "sqlite"
+           :subname     "database.db"
+           :user        "best5"
+           :password    ""})
 
-(def schema {})
+(defqueries "sql/queries.sql" {:connection conn})
 
-(def db (d/empty-db schema))
-
-(def conn (d/conn-from-db db))
-
-(defonce initial-data
-  (stock/getStockLists))
-
-(d/transact! conn
-             (into []
-                   initial-data))
-
-(d/q '[:find ?n
-       :where
-       [?e :代號 "2330"]
-       [?e :名稱 ?n]]
-     @conn)
-
-(d/q '[:find ?n
-       :where
-       [?e :代號 "6488"]
-       [?e :名稱 ?n]]
-     @conn)
-
-(d/q '[:find ?n
-       :where
-       [?e :代號 "2330"]
-       [?e :市場別 ?n]]
-     @conn)
-
-(defn save!
-  "Save db to PATH."
-  [path]
-  )
-
-(defn load! [path]
-  )
+(defn setup-db!
+  []
+  (best5-setup-db!))
